35bca90b52ba622f7344eec067f9cb296138595d
Sebas Willemse <sfwillemse@tudelft.nl>
2025-10-03 20:31:07 +0200

    Add rectangular_window(start_time, end_time,...) for gating ; keep generate_sine_wave as-is

--- signals.py
+++ signals.py
@@ -4,3 +4,9 @@ def generate_sine_wave(frequency, duration, sample_rate):
     N = int(round(duration*sample_rate)) #nmr of discrete samples
     t = np.linspace(0, duration, N, endpoint=False) # 0, 1/sample_rate, ... , (N-1)/sample_rate
     return np.sin(2 * np.pi * frequency * t)
+
+def rectangular_window(start_time, end_time, duration, sample_rate):
+    N = int(round(duration*sample_rate)) 
+    t = np.linspace(0, duration, N, endpoint=False)
+    return ((t>=start_time) & (t<end_time)).astype(float)
+    
