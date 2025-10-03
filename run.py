2f22469183dbccd2baf0e2a4db3f93e583420be2
Sebas Willemse <sfwillemse@tudelft.nl>
2025-10-03 12:07:39 +0200

    Plot a 5 Hz wave using linspace timebase endpoint= False

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 from signals import generate_sine_wave 
 
 def main():
@@ -6,7 +10,21 @@ def main():
     sample_rate = 100 
 
     y = generate_sine_wave(frequency, duration, sample_rate)
-    print(y[:10])
+
+    N = int(round(duration * sample_rate))
+    t = np.linspace(0, duration, N, endpoint=False)
+
+    plt.figure()
+    plt.plot(t,y)
+    plt.title(f"{frequency} Hz sine wave")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    
+
+
 
 if __name__ == "__main__": 
     main()
