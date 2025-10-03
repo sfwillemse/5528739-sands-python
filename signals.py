e0d252c56d83a80f40630061920a9b1ea1b12567
Sebas Willemse <sfwillemse@tudelft.nl>
2025-10-03 17:45:54 +0200

    Clarifying signals.py with docstring

--- signals.py
+++ signals.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def generate_sine_wave(frequency, duration, sample_rate):
-    N = int(round(duration*sample_rate))
-    t = np.linspace(0, duration, N, endpoint=False)
+    N = int(round(duration*sample_rate)) #nmr of discrete samples
+    t = np.linspace(0, duration, N, endpoint=False) # 0, 1/sample_rate, ... , (N-1)/sample_rate
     return np.sin(2 * np.pi * frequency * t)
